0ce65ec866d3e82d06d4a293885b1f2d61fd03aa
michalosman <michal.osman.997@gmail.com>
2020-10-07 17:07:22 +0200

    Initial commit (finish layout)

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
+    default:
+      return;
+  }
+}
